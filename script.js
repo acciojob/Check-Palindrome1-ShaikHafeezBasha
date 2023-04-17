d53db88bfbfc56ca5806a392c4fa4a40f07fe4cc
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-04-17 15:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+	const len=str.length;
+	for(let i=0;i<len;i++){
+		if(str[i]!=str[len-1-i]){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
